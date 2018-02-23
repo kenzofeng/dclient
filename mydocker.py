cd38358420fa62081f8885a8053ae821a5b9229a
daniel.liu <daniel.liu@derbysofot.com>
2018-02-23 09:17:03 +0800

    update docker clinet

--- mydocker.py
+++ mydocker.py
@@ -1,9 +1,8 @@
-from docker import APIClient
+import docker
 
-client = APIClient(base_url='unix://var/run/docker.sock')
 
-print (client)
 def build_images(projectpath, dockerfile):
-    global client
+    # client = APIClient(base_url='unix://var/run/docker.sock')
+    client = docker.APIClient(base_url='npipe:////./pipe/docker_engine')
     for line in client.build(path=projectpath, fileobj=dockerfile, tag="1.2.2"):
         print(line)
