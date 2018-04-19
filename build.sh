9901298b7fd3d4cd6a108ce38bfdbcc5c8b993e1
daniel.liu <daniel.liu@derbysofot.com>
2018-04-19 10:57:16 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #on windows
-pyinstaller --add-binary venv/Lib/site-packages/PyQt5/Qt/plugins/styles/qwindowsvistastyle.dll;PyQt5/Qt/plugins/style
-s/  -F  main.py
+pyinstaller --add-binary venv/Lib/site-packages/PyQt5/Qt/plugins/styles/qwindowsvistastyle.dll;PyQt5/Qt/plugins/styles/  -F  main.py
 #on linux or mac
-pyinstaller --add-binary venv/Lib/site-packages/PyQt5/Qt/plugins/styles/qwindowsvistastyle.dll:PyQt5/Qt/plugins/style
-s/  -F  main.py
+pyinstaller --add-binary venv/Lib/site-packages/PyQt5/Qt/plugins/styles/qwindowsvistastyle.dll:PyQt5/Qt/plugins/styles/  -F  main.py
